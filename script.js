1e54827520f5efcd9594e268190a680949f87b91
Felix Saint-Jore <felix.saintjore@gmail.com>
2023-03-06 14:05:31 +0100

    cleanup 4

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ https://www.youtube.com/watch?v=rTVoyWu8r6g
 
 */
 
-var styleSheet = document.createElement("style")
-styleSheet.innerText = styles
-document.head.appendChild(styleSheet)
-
-
-const canvas = document.getElementById("canvasGame");
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-const speed = 10;
-const timer = Date.now;
 
-
-canvas.width = window.innerWidth * 0.9;
+canvas.width = 800;
 canvas.height = 500;
 
 const scaleFactor = 2;
@@ -25,14 +17,12 @@ const scaledCanvas = {
     height: canvas.height / scaleFactor
 }
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 const GLB_gravity = .7;
 let GLB_velocityX = 5;
 let GLB_velocityY = 15;
 
 const GLB_bgColor = '#35daf0';
+
 class Sprite {
     constructor({position, velocity, persoImg}) {
         this.position = position;
@@ -61,54 +51,6 @@ class Sprite {
         }
     }
 }
-
-class obstacle {
-    constructor({position, width, height}) {
-        this.position = position;
-        //this.velocity = velocity;
-        this.width = width;
-        this.height = height;
-    }
-    draw() {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-
-    update() {
-        if (this.position.x + this.width  >= 0) {
-            this.draw();
-            this.position.x -= speed*0.1;
-        }
-        else {
-            return 0;
-        }
-        
-
-    }
-
-}
-
-
-class SpriteBackground {
-    constructor({position, imageSrc}) {
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc[0];
-
-    }
-
-    draw() {
-        if(!this.image) return
-        ctx.drawImage(this.image, this.position.x, this.position.y);
-
-    }
-
-    update() {
-        this.draw();
-        this.position.x -= speed*0.1;
-    }
-}
-
 const player = new Sprite({
     position: {
         x: 0,
@@ -116,55 +58,91 @@ const player = new Sprite({
     }, velocity: {
         x: 0,
         y: 0
-    }, 
+    },
     persoImg: './img/perso.png'
 });
+player.draw();
+console.log(player);
 
-
-const block2 = new obstacle({
-    position: {
-        x: 200,
-        y: 300
-    },
-    width: 40,
-    height: 15
-});
-
-const block = new obstacle({
-    position: {
-        x: 100,
-        y: 200
-    },
-    width: 50,
-    height: 30
-})
-// need to add a queue to make the background
-
-
-class ObstaclesOnScreen {
-    constructor(listeObstacle) {
-        this.listeObstacle = listeObstacle;
+class Plateforme{
+    constructor({position, platef}){
+        this.position=position;
+        this.image = new Image();
+        this.image.src = platef;
     }
     draw() {
-        console.log("obstacles should be drawing now")
-        this.listeObstacle.forEach(element => {
-            element.draw();
-        })
+        if(!this.image) return
+        ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+    update() {
+        this.draw(); 
+    }
+
+}
+const plateforme1 = new Plateforme({
+    position: {
+        x: 400,
+        y: 400,
+        width: 166,
+        height: 27
+    },
+    platef: './img/plateforme1.png'
+})
+
+class Obstacle{
+    constructor({position,velocity, obs}){
+        this.position=position;
+        this.velocity = velocity;
+        this.image = new Image();
+        this.image.src = obs;
+    }
+    draw() {
+        if(!this.image) return
+        ctx.drawImage(this.image, this.position.x, this.position.y);
     }
 
     update() {
-        this.listeObstacle.forEach(element => {
-            element.update();
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
 
-        })
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        }else{
+            this.velocity.y += GLB_gravity;
+        }
     }
- } 
 
-const tableObstacle = [block, block2];
-const obsOnScreen = new ObstaclesOnScreen(tableObstacle);
+}
+const obstaclepique = new Obstacle({
+    position: {
+        x: 600,
+        y: 450
+    }, velocity: {
+        x: 0,
+        y: 0
+    },
+    obs: './img/obspique.png'
+})
 
-obsOnScreen.draw();
-player.draw();
+
+class SpriteBackground {
+    constructor({position, imageSrc}) {
+        this.position = position;
+        this.image = new Image();
+        this.image.src = imageSrc;
+    }
+
+    draw() {
+        if(!this.image) return
+        ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+
+    update() {
+        this.draw();
+    }
+}
+
 
 
 const keys = {
@@ -177,7 +155,6 @@ const keys = {
     d: {
         pressed: false
     },
-
     arrowUp: {
         pressed: false
     },
@@ -186,9 +163,6 @@ const keys = {
     },
     arrowRight: {
         pressed: false
-    },
-    arrowDown: {
-        pressed : false
     }
 }
 
@@ -197,9 +171,9 @@ const background = new SpriteBackground({
         x: 0,
         y: 0
     },
-    imageSrc: ['./img/background.png']
+    imageSrc: './img/background.png'
 })
-//create an class with all the positions for the blocks + check with each move that we are not colliding 
+
 function animate() {
     window.requestAnimationFrame(animate);
     //ctx.fillStyle = 'black';
@@ -212,9 +186,12 @@ function animate() {
     ctx.translate(0, -background.image.height + scaledCanvas.height);
     background.update();
     ctx.restore();
-
+    /*plateformes*/
+    plateforme1.update();
+    /*obbstacles*/
+    obstaclepique.update();
+    /*player*/
     player.update();
-    obsOnScreen.update();
 
     player.velocity.x = 0;
 
@@ -232,14 +209,9 @@ function animate() {
     if (keys.q.pressed|| keys.arrowLeft.pressed) {
         player.velocity.x = -GLB_velocityX;
     }
-    if (keys.d.pressed || keys.arrowRight.pressed ) {
+    if (keys.d.pressed || keys.arrowRight.pressed) {
         player.velocity.x = GLB_velocityX;
     }
-    if (keys.arrowDown.pressed && player.position.y <= canvas.height) {
-        player.velocity.y = GLB_velocityY;
-    }
-
-    //creer une sorte de garbage collector pour quand les obstacles ne sont plus visible
 }
 
 animate();
@@ -270,10 +242,6 @@ window.addEventListener('keydown', (event) => {
             keys.arrowRight.pressed = true;
             player.lastKey = 'ArrowRight';
             break;
-        case 'ArrowDown' : 
-            keys.arrowDown.pressed = true;
-            player.lastKey = 'ArrowDown';
-            break;
     }
 });
 
@@ -297,12 +265,5 @@ window.addEventListener('keyup', (event) => {
         case 'ArrowRight':
             keys.arrowRight.pressed = false;
             break;
-        case 'ArrowDown':
-            keys.arrowDown.pressed = false;
-            break;
     }
 });
-
-
-
- 
