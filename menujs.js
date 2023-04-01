be9dd5c8ec8f11a770e6c3242c73c75abe041300
KEANU987 <58613754+KEANU987@users.noreply.github.com>
2023-04-01 08:05:26 +0000

    OurGame

--- menujs.js
+++ menujs.js
@@ -1,14 +1,38 @@
 bgMusic = document.getElementById("bgAudio");
-imgMusic = document.getElementById("btnSonImg")
-bgMusic.volume = 0.80;
+imgMusic = document.getElementById("btnSonImg");
+bgMusic.volume = 0.75;
+
+imgBlackSkin = document.getElementById("imgBlackSkin");
+btnBlackSkin = document.getElementById("btnBlackSkin");
+
+imgRedSkin = document.getElementById("imgRedSkin");
+btnRedSkin = document.getElementById("btnRedSkin");
+
+imgBluekSkin = document.getElementById("imgBluekSkin");
+btnBlueSkin = document.getElementById("btnBlueSkin");
 
 function change_mute() {
-  if(bgMusic.muted==false){
-    bgMusic.muted=true;
-    imgMusic.src="./img/sonoff.svg"
-  }else{
-    bgMusic.muted=false;
-    imgMusic.src="./img/sonon.svg"
+  if (bgMusic.muted == false) {
+    bgMusic.muted = true;
+    imgMusic.src = "./img/sonoff.svg"
+  } else {
+    bgMusic.muted = false;
+    imgMusic.src = "./img/sonon.svg"
   }
 
 }
+btnBlackSkin.addEventListener("click", () => {
+  imgBlackSkin.style.display = "block";
+  imgRedSkin.style.display = "none";
+  imgBluekSkin.style.display = "none";
+});
+btnRedSkin.addEventListener("click", () => {
+  imgRedSkin.style.display = "block";
+  imgBlackSkin.style.display = "none";
+  imgBluekSkin.style.display = "none";
+});
+btnBlueSkin.addEventListener("click", () => {
+  imgBluekSkin.style.display = "block";
+  imgBlackSkin.style.display = "none";
+  imgRedSkin.style.display = "none";
+});
--- script.js
+++ script.js
@@ -7,35 +7,46 @@ https://www.youtube.com/watch?v=rTVoyWu8r6g
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const speed = 10;
-canvas.width = 800;
+canvas.width = 1024;
 canvas.height = 500;
 
-const scaleFactor = 2;
+const scaleFactor = 1; //scale for bg
 
 const scaledCanvas = {
     width: canvas.width / scaleFactor,
     height: canvas.height / scaleFactor
 }
 
+const flour = 0  ;
 const GLB_gravity = .7;
 let GLB_velocityX = 5;
 let GLB_velocityY = 15;
 const GLB_bgColor = '#35daf0';
 
 class Sprite {
-    constructor({position, velocity, persoImg}) {
+    constructor({ position, velocity, persoImg, animations }) {
         this.position = position;
         this.velocity = velocity;
+        this.spriteWidth = 65;
+        this.spriteHeight = 65;
         this.image = new Image();
         this.image.src = persoImg;
-        this.width = 47;
-        this.height = 159;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+        this.minFrame = 0;
+        this.maxFrame = 355;
+        this.frame = 0;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.frameBuffer = 8;
+        this.elapsedFrame = 0;
         this.lastKey;
-    }
+        this.animations = animations;
+    }   
 
     draw() {
-        if(!this.image) return
-        ctx.drawImage(this.image, this.position.x, this.position.y);
+        if (!this.image) return
+        ctx.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.position.x, this.position.y, this.width, this.height);
     }
 
     update() {
@@ -43,13 +54,36 @@ class Sprite {
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
-        if(this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0;
-        }else{
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = flour;
+        } else {
             this.velocity.y += GLB_gravity;
+        }
+
+
+        /*if(this.frameX < 8) this.frameX++;
+        else 
+        this.frameX = 0;*/
+        this.elapsedFrame++;
+        if (this.elapsedFrame % this.frameBuffer === 0) {
+            if (this.frame < this.maxFrame) this.frame++;
+            else this.frame = this.minFrame;
+            this.frameX = this.frame % 8;
         }
     }
 }
+const playerDontMove = new Image();
+playerDontMove.src = './img/persodontmove.png';
+
+const playerRight = new Image();
+playerRight.src = './img/persoright.png';
+
+const playerJump = new Image();
+playerJump.src = './img/persojump.png';
+
+const playerLeft = new Image();
+playerLeft.src = './img/persoleft.png';
+
 const player = new Sprite({
     position: {
         x: 0,
@@ -58,26 +92,79 @@ const player = new Sprite({
         x: 0,
         y: 0
     },
-    persoImg: './img/perso.png'
+    persoImg: './img/persodontmove.png',
+    frameRate: 8,
+    animations: {
+        static: playerDontMove,
+        right: playerRight,
+        jump: playerJump,
+        left: playerLeft
+    }
 });
-player.draw();
-console.log(player);
 
-class Plateforme{
-    constructor({position, velocity, platef}){
-        this.position=position;
+const flourLayer = new Image();
+flourLayer.src = './img/flour.png'; //first layer
+const routeLayer = new Image();
+routeLayer.src = './img/route.png'; //first layer
+const batimentLayer = new Image();
+batimentLayer.src = './img/batiment.png';
+const rocheLayer = new Image();
+rocheLayer.src = './img/roche.png';
+const skyLayer = new Image();
+skyLayer.src = './img/sky.png';
+const ovniLayer = new Image();
+ovniLayer.src = './img/ovni.png'; //last layer
+
+class SpriteBackground{
+    constructor(image, speedBuffer){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 500;
+        this.x2 = this.width;
+        this.speedBuffer = speedBuffer;
+        this.speedy = this.speed * this.speedBuffer;
+        this.image = image;
+    }
+    update(){
+        this.speedy = speed * this.speedBuffer; 
+        if(this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speedy;
+        }
+        if(this.x2 <= -this.width){
+            this.x2  = this.width + this.x - this.speedy;
+        }
+        this.x = Math.floor(this.x - this.speedy);
+        this.x2 = Math.floor(this.x2 - this.speedy);
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
+    }
+}
+const layer1 = new SpriteBackground(flourLayer,0.6);
+const layer2 = new SpriteBackground(routeLayer,0.5);
+const layer3 = new SpriteBackground(rocheLayer,0.4);
+const layer4 = new SpriteBackground(batimentLayer,0.2);
+const layer5 = new SpriteBackground(skyLayer,0.1);
+const layer6 = new SpriteBackground(ovniLayer,0.3);
+
+
+class Plateforme {
+    constructor({ position, velocity, platef }) {
+        this.position = position;
         this.velocity = velocity;
         this.image = new Image();
         this.image.src = platef;
     }
     draw() {
-        if(!this.image) return
+        if (!this.image) return
         ctx.drawImage(this.image, this.position.x, this.position.y);
     }
 
     update() {
         this.draw();
-        this.position.x -= speed*0.1;
+
     }
 
 }
@@ -91,24 +178,24 @@ const plateforme1 = new Plateforme({
         x: 0,
         y: 0
     },
-    platef: './img/plateforme1.png'
+    platef: ''
 })
 
-class Obstacle{
-    constructor({position,velocity, obs}){
-        this.position=position;
+class Obstacle {
+    constructor({ position, velocity, obs }) {
+        this.position = position;
         this.velocity = velocity;
         this.image = new Image();
         this.image.src = obs;
     }
     draw() {
-        if(!this.image) return
+        if (!this.image) return
         ctx.drawImage(this.image, this.position.x, this.position.y);
     }
 
     update() {
         this.draw();
-        this.position.x -= speed*0.1;
+        this.position.x -= speed * 0.1;
     }
 
 }
@@ -120,27 +207,8 @@ const obstaclepique = new Obstacle({
         x: 0,
         y: 0
     },
-    obs: './img/obspique.png'
+    obs: ''
 })
-
-
-class SpriteBackground {
-    constructor({position, imageSrc}) {
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc;
-    }
-
-    draw() {
-        if(!this.image) return
-        ctx.drawImage(this.image, this.position.x, this.position.y);
-    }
-
-    update() {
-        this.draw();
-        this.position.x -= speed*0.1;
-    }
-}
 
 
 
@@ -165,32 +233,37 @@ const keys = {
     }
 }
 
-const background = new SpriteBackground({
-    position: {
-        x: 0,
-        y: 0
-    },
-    imageSrc: './img/background.png'
-})
+
 
 function animate() {
-    window.requestAnimationFrame(animate);
-    //ctx.fillStyle = 'black';
-    ctx.fillStyle = GLB_bgColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //console.log('go');
-    ctx.save();
-    ctx.scale(scaleFactor, scaleFactor);
-    ctx.translate(0, -background.image.height + scaledCanvas.height);
-    background.update();
-    ctx.restore();
+
+    /*background*/
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    
+    layer5.draw();
+    layer5.update();
+    layer6.draw();
+    layer6.update();
+    layer4.draw();
+    layer4.update();
+    layer3.draw();
+    layer3.update();
+    layer2.update();
+    layer2.draw();
+    layer1.update();
+    layer1.draw();
+    
+    
+    requestAnimationFrame(animate);
+    /*player*/
+    player.update();
+    player.draw();
+
     /*plateformes*/
     plateforme1.update();
     /*obstacles*/
     obstaclepique.update();
-    /*player*/
-    player.update();
+
 
     player.velocity.x = 0;
 
@@ -202,24 +275,28 @@ function animate() {
         player.velocity.x = GLB_velocityX;
     }*/
 
-    if((keys.z.pressed || keys.arrowUp.pressed) && ((Math.round((player.position.y + player.height) /100) * 100) == canvas.height)) {
+    if ((keys.z.pressed || keys.arrowUp.pressed) && ((Math.round((player.position.y + player.height) / 100) * 100) == canvas.height)) {
         player.velocity.y = -GLB_velocityY;
-    }
-    if (keys.q.pressed|| keys.arrowLeft.pressed) {
+        player.image = player.animations.jump;
+    }else if (player.velocity.y ==flour){
+        player.image = player.animations.static;
+    }if (keys.q.pressed || keys.arrowLeft.pressed) {
         player.velocity.x = -GLB_velocityX;
-    }
-    if (keys.d.pressed || keys.arrowRight.pressed) {
+        player.image = player.animations.left;
+    }if (keys.d.pressed || keys.arrowRight.pressed) {
         player.velocity.x = GLB_velocityX;
+        player.image = player.animations.right;
     }
 }
 
 animate();
 
 window.addEventListener('keydown', (event) => {
-    switch(event.key) {
+    switch (event.key) {
         case 'z':
             keys.z.pressed = true;
             player.lastKey = 'z';
+            
             break;
         case 'q':
             keys.q.pressed = true;
@@ -245,7 +322,7 @@ window.addEventListener('keydown', (event) => {
 });
 
 window.addEventListener('keyup', (event) => {
-    switch(event.key) {
+    switch (event.key) {
         case 'z':
             keys.z.pressed = false;
             break;
